a60c9649b6299e10629033dba86b55dbee54ee63
VuKhaa <hovaten169686@gmail.com>
2023-12-15 04:02:02 +0700

    create Login.cs

---
+++ projectWinform/controller/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectWinform.controller
+{
+    internal class Login
+    {
+    }
+}
